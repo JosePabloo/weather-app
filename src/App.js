14865aa5ed7f5373ead8ad0b37a12d9c108fddd2
Jose Perales <joseperales@Joses-MacBook-Pro.local>
2018-05-26 16:40:44 -0500

    Added two inputs in Weather.js for City and Country. Also Created a button.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ import Weather from "./components/Weather";
 const API_KEY ="f7301b4d01cee38c215671e794c39de5";
 
 class App extends React.Component{
-    getWeather = () => {}
+    getWeather = async () => {
+        const api_call = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Montreal&mode=html&appid=${API_KEY}&units=metric');
+
+       const data =await api_call.json();
+       console.log(data);
+    }
+
     render(){
         return(
           <div>
--- src/components/form.js
+++ src/components/form.js
@@ -3,7 +3,11 @@ import React from "react";
 class Form extends React.Component{
     render(){
         return(
-         <div>Form Componet</div>
+            <form>
+                <input type="text" name="city" placeholder="city.." />
+                <input type="text" name="country" placeholder="country.." />
+                <button>get weather</button>
+            </form>
         );
     }
 };
